8cebed5f8596a911c463db25c720bf8eb493df7b
abhishek-rajput119 <abrajput119@gmail.com>
2023-11-29 00:25:32 +0530

    adding navbar style and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,80 @@
-import '../css/navbar.css'
-import React from 'react'
+// Navbar.js
 
-const Navbar = ({ onBubbleSortClick }) => {
+import React from 'react'
+import '../css/navbar.css'
+const Navbar = ({ generateRandomArray, handleSort, setAlgo, sorting }) => {
   return (
     <nav>
       <div className="navbar">
         <h1>Sorting Visualizer</h1>
-        <button onClick={onBubbleSortClick}>Bubble Sort</button>
+        <div className="buttons">
+          <button onClick={generateRandomArray} disabled={sorting}>
+            Randomize Array
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Bubble Sort
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Insertion Sort
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Selection Sort
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Quick Sort
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Merge Sort
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Shell Sort
+          </button>
+          <button
+            onClick={() => {
+              setAlgo('bubbleSort')
+              handleSort()
+            }}
+            disabled={sorting}
+          >
+            Change Size
+          </button>
+        </div>
       </div>
     </nav>
   )
